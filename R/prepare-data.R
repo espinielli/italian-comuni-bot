306c115a795b94cb62c84ccae15ec71932eee62f
Enrico Spinielli <enrico.spinielli@gmail.com>
2020-12-28 17:59:33 +0100

    rewritten data preparation script for new ISTAT files format/organization

--- R/prepare-data.R
+++ R/prepare-data.R
@@ -4,11 +4,13 @@ library(purrr)
 library(sf)
 
 download_istat <- FALSE
+istat_year <- "2020"
 
 if (download_istat) {
   # download the relevant shapefile from ISTAT https://www.istat.it/it/archivio/124086
   #    http://www.istat.it/storage/cartografia/confini_amministrativi/archivio-confini/non_generalizzati/2016/Limiti_2016_WGS84.zip
   # reviewed in 20201225:
+  # https://www.istat.it/it/archivio/222527
   # https://www.istat.it/storage/cartografia/confini_amministrativi/non_generalizzati/Limiti01012020.zip
 
   istat_base <- "http://www.istat.it/"
@@ -22,33 +24,41 @@ if (download_istat) {
     # str_glue("{yyyy}/", yyyy = istat_year) %>%
     str_c(istat_filename)
   download.file(istat_file, istat_destination)
-  unzip(istat_destination, exdir = "data/cucu/")
+  unzip(istat_destination, exdir = "data")
 }
 
-regioni <- sf::st_read("data/Limiti_2016_WGS84/Reg2016_WGS84/Reg_2016_WGS84.shp") %>%
-  sf::st_transform(crs = 4326) %>%
-  dplyr::mutate(bb = split(., 1:nrow(.)) %>% purrr::map(st_bbox))
+# "data/Limiti_2016_WGS84/Reg2016_WGS84/Reg_2016_WGS84.shp" %>%
+#   sf::st_read() %>%
+#   sf::st_transform(crs = 4326) %>%
+#   dplyr::select(-starts_with("SHAPE")) %>%
+#   st_write("data/regions2016.geojson")
 
-rr <- regioni %>%
-  dplyr::select(COD_REG, REGIONE) %>%
+
+regioni <- "data/Limiti0101{yyyy}/Reg0101{yyyy}/Reg0101{yyyy}_WGS84.shp" %>%
+  str_glue(yyyy = istat_year) %>%
+  sf::st_read() %>%
+  sf::st_transform(crs = 4326) %>%
+  dplyr::mutate(bb = split(., 1:nrow(.)) %>% purrr::map(st_bbox)) %>%
+  dplyr::select(COD_REG, DEN_REG) %>%
+  st_write(str_glue("data/regions{yyyy}.geojson", yyyy = istat_year)) %>%
   sf::st_set_geometry(NULL)
 
-province <- st_read("data/Limiti_2016_WGS84/CMProv2016_WGS84/CMprov2016_WGS84.shp") %>%
+province <- "data/Limiti0101{yyyy}/ProvCM0101{yyyy}/ProvCM0101{yyyy}_WGS84.shp" %>%
+  str_glue(yyyy = istat_year) %>%
+  st_read() %>%
   sf::st_transform(crs = 4326) %>%
-  dplyr::mutate(bb = split(., 1:nrow(.)) %>% purrr::map(st_bbox))
-
-pp <- province %>%
-  dplyr::select(COD_REG, COD_PRO, DEN_CMPRO, PROVINCIA, SIGLA) %>%
+  dplyr::mutate(bb = split(., 1:nrow(.)) %>% purrr::map(st_bbox)) %>%
+  dplyr::select(COD_REG, COD_PROV, DEN_PCM, DEN_PROV, SIGLA) %>%
   sf::st_set_geometry(NULL)
 
-comuni <- sf::st_read("data/Limiti_2016_WGS84/Com2016_WGS84/Com2016_WGS84.shp") %>%
+comuni <- "data/Limiti0101{yyyy}/Com0101{yyyy}/Com0101{yyyy}_WGS84.shp" %>%
+  str_glue(yyyy = istat_year) %>%
+  sf::st_read() %>%
   sf::st_transform(crs = 4326) %>%
-  dplyr::mutate(bb = split(., 1:nrow(.)) %>% purrr::map(st_bbox))
-
-coms <- comuni %>%
-  dplyr::left_join(rr) %>%
-  dplyr::left_join(pp) %>%
-  dplyr::select(-FLAG_CM, -NOME_TED, -starts_with("SHAPE")) %>%
+  dplyr::mutate(bb = split(., 1:nrow(.)) %>% purrr::map(st_bbox)) %>%
+  dplyr::left_join(regioni) %>%
+  dplyr::left_join(province) %>%
+  dplyr::select(-starts_with("SHAPE")) %>%
   dplyr::arrange(PRO_COM)
 
 saveRDS(coms, "data/coms.rds")
